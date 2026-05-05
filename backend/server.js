b7326518df8f2cd9f8a4fdb5ddfc9eee579ce2b7
Shashank <kashashank129@gmail.com>
2026-05-05 18:13:56 +0000

    backend ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,15 @@ app.use(express.json());
 /* =========================
    MongoDB Connection
 ========================= */
-mongoose.connect("mongodb://127.0.0.1:27017/iot-health")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 
 /* =========================
    Schemas
